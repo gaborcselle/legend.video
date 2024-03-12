77030e6de5f63e60bb18648dd30db391f3a1ed41
Lautaro Pereyra <hi@imlautaro.com>
2024-03-12 11:18:39 -0300

    fix and refactor export endpoint

--- app/api/export_project/[id]/route.ts
+++ app/api/export_project/[id]/route.ts
@@ -2,35 +2,22 @@ import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/middleware'
 import axios from 'axios'
 import archiver from 'archiver'
-import { createWriteStream, createReadStream } from 'fs'
+import { SupabaseClient } from '@supabase/supabase-js'
+import { PassThrough } from 'stream'
 
-// TODO: We need to refactor this to use in-memory streams
-//       instead of writing to disk
-export async function GET(request: NextRequest, { params }) {
-  const { supabase } = createClient(request)
-
-  const id = params.id
-
-  const { data: project } = await supabase
-    .from('projects')
-    .select('id, title')
-    .eq('id', id)
-    .single()
-
-  if (!project) return NextResponse.json({ error: 'Failed to fetch project' })
-
+const getSceneData = async (supabase: SupabaseClient, projectId: string) => {
   // Fetch scenes
   const { data: scenes } = await supabase
     .from('scenes')
     .select('id, seq_num, title')
     .order('seq_num', { ascending: true })
-    .eq('project_id', id)
+    .eq('project_id', projectId)
 
-  if (!scenes) return NextResponse.json({ scenes: [] })
+  if (!scenes) return null
 
   // Fetch shots for each scene
   const data = await Promise.all(
-    scenes.map(async (scene) => {
+    scenes.map(async scene => {
       const { data: shots } = await supabase
         .from('shots')
         .select('id, seq_num, title')
@@ -41,7 +28,7 @@ export async function GET(request: NextRequest, { params }) {
 
       // Fetch shot prompts for each shot
       const shotsWithPrompts = await Promise.all(
-        shots.map(async (shot) => {
+        shots.map(async shot => {
           const { data: shotPrompts } = await supabase
             .from('shot_prompts')
             .select('id, seq_num, selected_still')
@@ -52,7 +39,7 @@ export async function GET(request: NextRequest, { params }) {
 
           // Fetch shot stills for each prompt
           const promptsWithStills = await Promise.all(
-            shotPrompts.map(async (prompt) => {
+            shotPrompts.map(async prompt => {
               const { data: shotStills } = await supabase
                 .from('shot_stills')
                 .select('id, seq_num, still_url, selected_video')
@@ -63,7 +50,7 @@ export async function GET(request: NextRequest, { params }) {
 
               // Fetch shot videos for each still
               const stillsWithVideos = await Promise.all(
-                shotStills.map(async (still) => {
+                shotStills.map(async still => {
                   const { data: shotVideos } = await supabase
                     .from('shot_videos')
                     .select('id, seq_num, video_url')
@@ -75,14 +62,14 @@ export async function GET(request: NextRequest, { params }) {
                     videos:
                       shotVideos && shotVideos[still.selected_video]
                         ? [shotVideos[still.selected_video]]
-                        : [],
+                        : []
                   }
                 })
               )
 
               return {
                 ...prompt,
-                stills: [stillsWithVideos[prompt.selected_still]],
+                stills: [stillsWithVideos[prompt.selected_still]]
               }
             })
           )
@@ -95,23 +82,47 @@ export async function GET(request: NextRequest, { params }) {
     })
   )
 
-  const urls = [] as object[]
-  data.forEach((scene) => {
-    scene.shots.forEach((shot) => {
-      shot.prompts.forEach((prompt) => {
-        prompt.stills.forEach((still) => {
+  return data
+}
+
+const extractUrls = (
+  data: {
+    id: number
+    shots: {
+      id: number
+      prompts: {
+        id: number
+        stills: {
+          id: number
+          still_url?: string
+          videos: {
+            id: number
+            video_url?: string
+          }[]
+        }[]
+      }[]
+    }[]
+  }[],
+  projectId: number
+) => {
+  const urls: { filename: string; url: string }[] = []
+
+  data.forEach(scene => {
+    scene.shots.forEach(shot => {
+      shot.prompts.forEach(prompt => {
+        prompt.stills.forEach(still => {
           if (still.still_url) {
             urls.push({
-              filename: `pj_${id}-sc_${scene.id}-sh_${shot.id}-pr_${prompt.id}-st_${still.id}`,
-              url: still.still_url,
+              filename: `pj_${projectId}-sc_${scene.id}-sh_${shot.id}-pr_${prompt.id}-st_${still.id}`,
+              url: still.still_url
             })
           }
           if (still.videos.length) {
-            still.videos.forEach((video) => {
+            still.videos.forEach(video => {
               if (video.video_url) {
                 urls.push({
-                  filename: `pj_${id}-sc_${scene.id}-sh_${shot.id}-pr_${prompt.id}-st_${still.id}-vi_${video.id}`,
-                  url: video.video_url,
+                  filename: `pj_${projectId}-sc_${scene.id}-sh_${shot.id}-pr_${prompt.id}-st_${still.id}-vi_${video.id}`,
+                  url: video.video_url
                 })
               }
             })
@@ -121,16 +132,35 @@ export async function GET(request: NextRequest, { params }) {
     })
   })
 
+  return urls
+}
+
+export async function GET(request: NextRequest, { params: { id: projectId } }) {
+  const { supabase } = createClient(request)
+
+  const { data: project } = await supabase
+    .from('projects')
+    .select('id, title')
+    .eq('id', projectId)
+    .single()
+
+  if (!project) return NextResponse.json({ error: 'Failed to fetch project' })
+
+  const data = await getSceneData(supabase, projectId)
+
+  if (!data) return NextResponse.json({ error: 'Failed to fetch scenes' })
+
+  const urls = extractUrls(data, projectId)
+
+  // Create an in-memory stream for the archive data
+  const archiveStream = new PassThrough()
+
   // Create a new zip archive
   const archive = archiver('zip', {
-    zlib: { level: 9 }, // Compression level (0-9)
+    zlib: { level: 9 } // Compression level (0-9)
   })
 
-  
-  // Create a writable stream for the archive
-  const writableStream = createWriteStream(`archive_${id}.zip`)
-
-  archive.pipe(writableStream);
+  archive.pipe(archiveStream)
 
   const addFileToArchive = async (url: string, filename: string) => {
     const response = await axios.get(url, { responseType: 'arraybuffer' })
@@ -138,30 +168,28 @@ export async function GET(request: NextRequest, { params }) {
   }
 
   await Promise.all(
-    urls.map(async (url) => {
+    urls.map(async url => {
       await addFileToArchive(
-        // @ts-ignore
         url.url,
-        // @ts-ignore
         `${url.filename}.${url.url.split('.').pop()}`
       )
     })
   )
 
-  archive.finalize();
+  archive.finalize()
 
-  const dataBuffer: Buffer[] = [];
-
-  // TODO(gabor): We need the fix this hack, ideally all of this data should be streamed
-  const readStream = createReadStream(`archive_${id}.zip`);
-  for await (const chunk of readStream) {
+  const dataBuffer: Buffer[] = []
+  for await (const chunk of archiveStream) {
     dataBuffer.push(chunk)
   }
+
   const buffer = Buffer.concat(dataBuffer)
 
-  // return the buffer as a response
   const response = new NextResponse(buffer)
-  response.headers.set('Content-Disposition', `attachment; filename="archive_${id}.zip"`)
+  response.headers.set(
+    'Content-Disposition',
+    `attachment; filename="archive_${projectId}.zip"`
+  )
   response.headers.set('Content-Type', 'application/zip')
 
   return response
